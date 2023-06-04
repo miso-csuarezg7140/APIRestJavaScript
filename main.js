e1d575cd7801bda32dacb570cba9869f5dfbad4a
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2023-06-04 12:26:22 -0500

    Se agrega endpoint get favoritos

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-console.log('Hello world')
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_FAxVzuV4jCq3XgjV2CSbV26bkx61XrUeJN1tQfnYf1aONKT1txhB1fK7XPbksYMB'
 
-const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=live_FAxVzuV4jCq3XgjV2CSbV26bkx61XrUeJN1tQfnYf1aONKT1txhB1fK7XPbksYMB'
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_FAxVzuV4jCq3XgjV2CSbV26bkx61XrUeJN1tQfnYf1aONKT1txhB1fK7XPbksYMB'
 
-async function reload() {
-    const res = await fetch(API_URL)
+const spanError = document.getElementById('Error')
+
+async function loadRandomCats() {
+    const res = await fetch(API_URL_RANDOM)
     const data = await res.json()
-
+    console.log('Random')
     console.log(data)
-    const img1 = document.getElementById('img1')
-    const img2 = document.getElementById('img2')
-    const img3 = document.getElementById('img3')
-    img1.src = data[0].url
-    img2.src = data[1].url
-    img3.src = data[2].url
+
+    if (res.status != 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status
+    } else {
+        const img1 = document.getElementById('img1')
+        const img2 = document.getElementById('img2')
+        img1.src = data[0].url
+        img2.src = data[1].url
+    }    
 }
 
-reload()
+async function loadFavoritesCats() {
+    const res = await fetch(API_URL_FAVORITES)
+    const data = await res.json()
+    console.log('Favourites')
+    console.log(data)
+
+    if (res.status != 200) {
+        spanError.innerHTML = "Hubo un error: "+ res.status + data.message
+    }
+}
+
+loadRandomCats()
+loadFavoritesCats()    
